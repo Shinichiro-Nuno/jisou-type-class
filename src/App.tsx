a23cceb00bc353af27f5b0319ad66bb3f0f5ec08
Shinichiro-Nuno <nuno1216shin@gmail.com>
2024-12-05 21:40:11 +0900

    supabaseからデータ取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import { Button } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { GetAllTodos } from "./lib/todo";
+import { Todo } from "./domain/todo";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  useEffect(() => {
+    const getAllTodos = async () => {
+      const todosData = await GetAllTodos();
+      setTodos(todosData);
+    };
+
+    getAllTodos();
+  }, []);
+
   return (
     <>
-      <Button colorPalette="teal">Button</Button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
